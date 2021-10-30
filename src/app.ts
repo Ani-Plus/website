6ed8918f6c7c53031b8164994b95560fcb671411
Falsis <keremtokul@hotmail.com>
2021-10-30 17:01:13 +0300

    feat: the most basic site has been created

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,22 @@ import render from "../scripts/renderPage";
 import logger from "../utils/scripts/logger";
 
 logger("Ani+ is Ready!")
-
-app.get("/", (req, res) => {
-    return res.send("hello world")
-    // return render("main", res)
+//////////////////////////////////
+app.set("view engine", "ejs");
+app.set("views", process.cwd() + "/views")
+//////////////////////////////////
+app.get("/", function(req, res) {
+    let test: string = "Test Text"
+    res.render("index", {test})
 })
-
+app.get("/style", function(req, res) {
+    res.sendFile(process.cwd() + "/utils/styles/style.css")
+})
+app.get("/logo", function(req, res) {
+    res.sendFile(process.cwd() + "/utils/images/logo.png")
+})
+app.get("/anime/:name", (req, res) => {
+    if(req.params.name === "owari-no-seraph") res.render(process.cwd() + "/views/animes/owari-no-seraph/index")
+})
+//////////////////////////////////
 app.listen(config.port)
