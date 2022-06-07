8bf692ab3a95e6c5c39ec6f2cc3992622bd2ce8b
Falsis <keremtokul@hotmail.com>
2022-06-07 19:56:15 +0300

    edit: important changes

--- src/app.ts
+++ src/app.ts
@@ -38,7 +38,8 @@ app.get("/anime/:name", (req, res) => {
       seasons: animedata[req.params.name]["anime"]["seasons"],
       seasonCount: animedata[req.params.name].anime.seasoncount
     }
-    res.render(process.cwd() + "/views/anime", {anime})
+    let n = req.params.name
+    res.render(process.cwd() + "/views/anime", {anime, n})
 })
 app.get("/anime/watch/:name/:season/:episode", (req, res) => {
   if(!animedata[req.params.name]) res.json({
@@ -61,6 +62,25 @@ app.get("/anime/watch/:name/:season/:episode", (req, res) => {
     let ep1 = episode + 1
     let ep2 = episode - 1
     res.render(process.cwd() + "/views/watch", {anime, season, episode, dat, n, ep1, ep2})
+})
+app.get("/anime/:name/season/:season/", (req, res) => {
+  if(!animedata[req.params.name]) res.json({
+    response: "Böyle bir sayfa bulunamadı."
+  })
+  let anime = {
+    name: animedata[req.params.name]["names"]["en"],
+    types: animedata[req.params.name]["manga"]["genres"].join("/"),
+    description: animedata[req.params.name]["descriptions"]["tr"],
+    cover: animedata[req.params.name]["manga"]["covers"]["last"],
+    background: animedata[req.params.name]["design"].banner || "https://animecix.net/client/assets/images/default_episode_poster.jpg",
+    year: animedata[req.params.name]["anime"]["seasons"][req.params.season]["date"].split(" ")[2],
+    seasons: animedata[req.params.name]["anime"]["seasons"],
+    seasonCount: animedata[req.params.name].anime.seasoncount
+  }
+    let season = req.params.season
+    let dat = animedata[req.params.name].anime.seasons
+    let n = req.params.name
+    res.render(process.cwd() + "/views/season", {anime, season, dat, n})
 })
 //////////////////////////////////
 app.listen(config.port)
